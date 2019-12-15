45473a67dbcf45f01701bd5aed870946e6a89b85
Mike Williams <mike.williams@greensync.com>
2019-12-15 21:05:25 +1100

    Refactor.

--- lib/green_log/entry.rb
+++ lib/green_log/entry.rb
@@ -8,17 +8,13 @@ module GreenLog
   # Represents a structured log entry.
   class Entry < Value.new(:severity, :message, :context, :data, :exception)
 
-    def self.with(
-      severity: nil, message: nil,
-      context: nil, data: nil, exception: nil
-    )
-      super(
-        severity: Severity.resolve(severity || Severity::INFO),
-        message: message,
-        context: (context || {}),
-        data: (data || {}),
-        exception: exception,
-      )
+    def self.with(**args)
+      args[:severity] = Severity.resolve(args.fetch(:severity, Severity::INFO))
+      args[:message] ||= nil
+      args[:context] ||= {}
+      args[:data] ||= {}
+      args[:exception] ||= nil
+      super(**args)
     end
 
     def with_context(extra_context)
