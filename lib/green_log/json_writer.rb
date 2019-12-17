fda2ea944cf7ff87a73c98bc6e4646a8c2926f82
Mike Williams <mike.williams@greensync.com>
2019-12-17 17:40:03 +1100

    Make a JsonWriter.

---
+++ lib/green_log/json_writer.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "green_log/entry"
+require "green_log/severity"
+
+module GreenLog
+
+  # A JSON-formated log.
+  class JsonWriter
+
+    def initialize(io)
+      @io = io
+    end
+
+    attr_reader :io
+
+    def <<(entry)
+      raise ArgumentError, "GreenLog::Entry expected" unless entry.is_a?(GreenLog::Entry)
+
+      record = {
+        "severity" => Severity.name(entry.severity).upcase,
+        "message" => entry.message,
+        "data" => entry.data,
+        "context" => entry.context
+      }
+      io << JSON.dump(record) + "\n"
+    end
+
+  end
+
+end
---
+++ spec/green_log/json_writer_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require "green_log/entry"
+require "green_log/severity"
+require "green_log/json_writer"
+require "json"
+
+RSpec.describe GreenLog::JsonWriter do
+
+  let(:buffer) { StringIO.new }
+  let(:writer) { described_class.new(buffer) }
+
+  def output
+    buffer.string
+  end
+
+  def output_data
+    JSON.parse(buffer.string.each_line.first)
+  end
+
+  def log(**args)
+    writer << GreenLog::Entry.with(**args)
+  end
+
+  describe "#<<" do
+
+    context "with a simple message" do
+
+      let(:message) { "You've been warned" }
+
+      before do
+        log(severity: "WARN", message: message)
+      end
+
+      it "includes the message" do
+        expect(output_data).to include("message" => message)
+      end
+
+      it "includes severity" do
+        expect(output_data).to include("severity" => "WARN")
+      end
+
+    end
+
+  end
+
+  context "with context" do
+
+    before do
+      log(context: { thread: "main" })
+    end
+
+    it "includes the message" do
+      expect(output_data).to include(
+        "context" => {
+          "thread" => "main"
+        }
+      )
+    end
+
+  end
+
+  context "with data" do
+
+    before do
+      log(data: { duration: 42 })
+    end
+
+    it "includes the message" do
+      expect(output_data).to include(
+        "data" => {
+          "duration" => 42
+        }
+      )
+    end
+
+  end
+
+end
