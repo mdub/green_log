5976e1f8a0f29c228286ceeab0d80eb62b5d6bc0
Mike Williams <mike.williams@greensync.com>
2019-12-24 16:26:14 +1100

    Rack request logging.

---
+++ lib/green_log/rack/request_logging.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require "rack"
+
+module GreenLog
+
+  module Rack
+
+    # Structured request logging.
+    class RequestLogging
+
+      def initialize(app, logger)
+        @app = app
+        @logger = logger
+      end
+
+      def call(env)
+        started_at = Time.now
+        status, headers, _body = @app.call(env)
+      ensure
+        status ||= 500
+        headers ||= {}
+        duration = Time.now - started_at
+        log(
+          request: ::Rack::Request.new(env),
+          response: Response.new(status, headers, duration)
+        )
+      end
+
+      Response = Struct.new(:status, :headers, :duration)
+
+      private
+
+      attr_reader :logger
+
+      def log(request:, response:)
+        logger.info(
+          "#{request.request_method} #{request.path} #{response.status}",
+          request: request_details(request: request),
+          response: response_details(response: response)
+        )
+      end
+
+      def request_details(request:)
+        {
+          method: request.request_method,
+          scheme: request.scheme,
+          host: request.host,
+          path: request.path,
+          query: request.query_string,
+          remote_user: request.env["REMOTE_USER"],
+          remote_addr: request.ip
+        }
+      end
+
+      def response_details(response:)
+        {
+          status: response.status.to_i,
+          length: response.headers["Content-Length"].to_i,
+          duration: response.duration.round(3)
+        }
+      end
+
+    end
+
+  end
+
+end
---
+++ spec/green_log/rack/request_logging_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "green_log/logger"
+require "green_log/rack/request_logging"
+
+RSpec.describe GreenLog::Rack::RequestLogging do
+
+  let(:entries) { [] }
+
+  subject(:logger) { GreenLog::Logger.new(entries) }
+
+  subject(:middleware) { described_class.new(app, logger) }
+
+  let(:ip) { "123.456.789.001" }
+  let(:user_name) { "Robert" }
+  let(:request_host) { "api.example.com" }
+  let(:request_method) { "GET" }
+  let(:request_path) { "/whoami" }
+  let(:request_scheme) { "https" }
+
+  let(:request_env) do
+    {
+      "HTTP_HOST" => request_host,
+      "HTTP_X_FORWARDED_PROTO" => request_scheme,
+      "PATH_INFO" => request_path,
+      "QUERY_STRING" => "",
+      "REMOTE_ADDR" => ip,
+      "REMOTE_USER" => user_name,
+      "REQUEST_METHOD" => request_method
+    }
+  end
+
+  let(:response_status) { 200 }
+
+  let(:app) do
+    lambda do |env|
+      visitor = env.fetch("REMOTE_USER", "stranger")
+      message = "Hello, #{visitor}\n"
+      [response_status, { "Content-Length" => message.length }, [message]]
+    end
+  end
+
+  def make_request
+    middleware.call(request_env)
+  end
+
+  describe "#call" do
+
+    before { make_request }
+
+    context "usually" do
+
+      it "includes basics details in log message" do
+        expect(entries.last.message).to eq("#{request_method} #{request_path} #{response_status}")
+      end
+
+      it "includes request details" do
+        expect(entries.last.data).to include(
+          request: hash_including(
+            remote_addr: ip,
+            remote_user: user_name,
+            method: request_method,
+            scheme: request_scheme,
+            host: request_host,
+            path: request_path,
+            query: ""
+          )
+        )
+      end
+
+      it "includes response details" do
+        expect(entries.last.data).to include(
+          response: hash_including(
+            status: response_status,
+            length: Integer,
+            duration: Float
+          )
+        )
+      end
+
+    end
+
+  end
+
+end
